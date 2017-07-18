768b3611243874fe8db40564d6f5a9a235ae2ea5
gaotianye <15156506755@163.com>
2017-07-18 23:05:51 +0800

    update:ip正则处理第二种方式

--- celloud.hadoop/src/main/java/dashuju/celloud/test/RegexOssLog.java
+++ celloud.hadoop/src/main/java/dashuju/celloud/test/RegexOssLog.java
@@ -13,9 +13,9 @@ import dashuju.celloud.hadoop.demo2.OssLogWritable;
 public class RegexOssLog {
 	public static void main(String[] args) {
 		OssLogWritable osslog = new OssLogWritable();
-//		String input = " 47.93.36.59 - - [06/Mar/2017:15:13:32 +0800] \"HEAD /logs_%24folder%24 HTTP/1.1\" 404 0 1 \"-\" \"aliyun-sdk-http/1.0()/ossfs1.79.8\" \"bioinfo-cluster-dev.oss-cn-beijing.aliyuncs.com\" \"58BD0C1C483412961E8C9FB3\" \"true\" \"1295372657422098\" \"HeadObject\" \"bioinfo-cluster-dev\" \"logs_%24folder%24\" - 1 \"NoSuchKey\" 254 \"1295372657422098\" - \"-\" \"-\"";
 		String input = " 47.93.36.59 - - [06/Mar/2017:15:13:32 +0800] \"HEAD /logs_%24folder%24 HTTP/1.1\" 404 0 1 \"-\" \"aliyun-sdk-http/1.0()/ossfs1.79.8\" \"bioinfo-cluster-dev.oss-cn-beijing.aliyuncs.com\" \"58BD0C1C483412961E8C9FB3\" \"true\" \"1295372657422098\" \"HeadObject\" \"bioinfo-cluster-dev\" \"logs_%24folder%24\" - 1 \"NoSuchKey\" 254 \"1295372657422098\" - \"-\" \"-\"";
-		String regex = " (\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}) (.+) (.+) \\[([^]]+)\\] \"([^\"]+)\" ([0-9]+) ([0-9]+) ([0-9]+) \"([^\"]+)\" \"([^\"]+)\" \"([^\"]+)\" \"([^\"]+)\" \"([^\"]+)\" \"([^\"]+)\" \"([^\"]+)\" \"([^\"]+)\" \"([^\"]+)\" (.+) (.+) \"([^\"]+)\" ([0-9]+) \"([^\"]+)\" (.+) \"([^\"]+)\" \"([^\"]+)\"";
+//		String regex = " (\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}) (.+) (.+) \\[([^]]+)\\] \"([^\"]+)\" ([0-9]+) ([0-9]+) ([0-9]+) \"([^\"]+)\" \"([^\"]+)\" \"([^\"]+)\" \"([^\"]+)\" \"([^\"]+)\" \"([^\"]+)\" \"([^\"]+)\" \"([^\"]+)\" \"([^\"]+)\" (.+) (.+) \"([^\"]+)\" ([0-9]+) \"([^\"]+)\" (.+) \"([^\"]+)\" \"([^\"]+)\"";
+		String regex = " ([^ ]+) (.+) (.+) \\[([^]]+)\\] \"([^\"]+)\" ([0-9]+) ([0-9]+) ([0-9]+) \"([^\"]+)\" \"([^\"]+)\" \"([^\"]+)\" \"([^\"]+)\" \"([^\"]+)\" \"([^\"]+)\" \"([^\"]+)\" \"([^\"]+)\" \"([^\"]+)\" (.+) (.+) \"([^\"]+)\" ([0-9]+) \"([^\"]+)\" (.+) \"([^\"]+)\" \"([^\"]+)\"";
 		Pattern pattern = Pattern.compile(regex);
 		Matcher matcher = pattern.matcher(input);
 		if(matcher.find()){
