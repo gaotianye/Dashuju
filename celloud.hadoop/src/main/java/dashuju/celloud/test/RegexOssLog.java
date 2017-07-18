4554189a45a75edfb8fcd821d14d4f36db403430
gaotianye <15156506755@163.com>
2017-07-18 23:02:35 +0800

    update:修改正则表达式，\\s可以用空格替换

--- celloud.hadoop/src/main/java/dashuju/celloud/test/RegexOssLog.java
+++ celloud.hadoop/src/main/java/dashuju/celloud/test/RegexOssLog.java
@@ -15,7 +15,7 @@ public class RegexOssLog {
 		OssLogWritable osslog = new OssLogWritable();
 //		String input = " 47.93.36.59 - - [06/Mar/2017:15:13:32 +0800] \"HEAD /logs_%24folder%24 HTTP/1.1\" 404 0 1 \"-\" \"aliyun-sdk-http/1.0()/ossfs1.79.8\" \"bioinfo-cluster-dev.oss-cn-beijing.aliyuncs.com\" \"58BD0C1C483412961E8C9FB3\" \"true\" \"1295372657422098\" \"HeadObject\" \"bioinfo-cluster-dev\" \"logs_%24folder%24\" - 1 \"NoSuchKey\" 254 \"1295372657422098\" - \"-\" \"-\"";
 		String input = " 47.93.36.59 - - [06/Mar/2017:15:13:32 +0800] \"HEAD /logs_%24folder%24 HTTP/1.1\" 404 0 1 \"-\" \"aliyun-sdk-http/1.0()/ossfs1.79.8\" \"bioinfo-cluster-dev.oss-cn-beijing.aliyuncs.com\" \"58BD0C1C483412961E8C9FB3\" \"true\" \"1295372657422098\" \"HeadObject\" \"bioinfo-cluster-dev\" \"logs_%24folder%24\" - 1 \"NoSuchKey\" 254 \"1295372657422098\" - \"-\" \"-\"";
-		String regex = "\\s(\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3})\\s(.+)\\s(.+)\\s\\[([^]]+)\\]\\s\"([^\"]+)\"\\s([0-9]+)\\s([0-9]+)\\s([0-9]+)\\s\"([^\"]+)\"\\s\"([^\"]+)\"\\s\"([^\"]+)\"\\s\"([^\"]+)\"\\s\"([^\"]+)\"\\s\"([^\"]+)\"\\s\"([^\"]+)\"\\s\"([^\"]+)\"\\s\"([^\"]+)\"\\s(.+)\\s(.+)\\s\"([^\"]+)\"\\s([0-9]+)\\s\"([^\"]+)\"\\s(.+)\\s\"([^\"]+)\"\\s\"([^\"]+)\"";
+		String regex = " (\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}) (.+) (.+) \\[([^]]+)\\] \"([^\"]+)\" ([0-9]+) ([0-9]+) ([0-9]+) \"([^\"]+)\" \"([^\"]+)\" \"([^\"]+)\" \"([^\"]+)\" \"([^\"]+)\" \"([^\"]+)\" \"([^\"]+)\" \"([^\"]+)\" \"([^\"]+)\" (.+) (.+) \"([^\"]+)\" ([0-9]+) \"([^\"]+)\" (.+) \"([^\"]+)\" \"([^\"]+)\"";
 		Pattern pattern = Pattern.compile(regex);
 		Matcher matcher = pattern.matcher(input);
 		if(matcher.find()){
