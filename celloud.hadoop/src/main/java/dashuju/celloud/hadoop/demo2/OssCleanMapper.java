162b8185cba91cd53c67d25341a17326d60aa80d
gaotianye <15156506755@163.com>
2017-07-19 08:50:43 +0800

    update：输出NullWritable的另外一种写法

--- celloud.hadoop/src/main/java/dashuju/celloud/hadoop/demo2/OssCleanMapper.java
+++ celloud.hadoop/src/main/java/dashuju/celloud/hadoop/demo2/OssCleanMapper.java
@@ -16,7 +16,7 @@ import org.apache.hadoop.mapreduce.Mapper;
  */
 public class OssCleanMapper extends Mapper<LongWritable, Text, NullWritable, OssLogWritable> {
 	private OssLogWritable osslog;
-	private NullWritable nullKey;
+//	private NullWritable nullKey;
 	@Override
 	protected void setup(Context context)
 			throws IOException, InterruptedException {
@@ -33,7 +33,8 @@ public class OssCleanMapper extends Mapper<LongWritable, Text, NullWritable, Oss
 		if(matcher.find()){
 			osslog = add2OssLog(matcher,osslog);
 		}
-		context.write(nullKey, osslog);
+//		context.write(nullkey, osslog);
+		context.write(NullWritable.get(), osslog);
 	}
 	/**
 	 * 构建osslog对象
