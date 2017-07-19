29f684d779d8822e3f81ab67cea619572acf21e2
gaotianye <15156506755@163.com>
2017-07-19 22:06:41 +0800

    add:去重及测试

---
+++ celloud.hadoop/src/main/java/dashuju/celloud/hadoop/demo3/NullWritableDemo.java
@@ -0,0 +1,94 @@
+package dashuju.celloud.hadoop.demo3;
+
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+/**
+ * MapReduce实现去重 NullWritable的使用
+ * 参考：http://www.makaidong.com/%E5%8D%9A%E5%AE%A2%E5%9B%AD%E6%8E%92%E8%A1%8C/18828.shtml
+ * 
+ * 思路：在reducer端聚合，而仅仅是为了去重，所以不需要value输出信息。
+ * @author Administrator
+ *
+ */
+public class NullWritableDemo extends Configured implements Tool  {
+
+	public int run(String[] args) throws Exception {
+		Configuration conf = new Configuration();
+		conf.set("fs.defaultFS", "hdfs://master:9000");
+		
+		String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
+		
+		if (otherArgs.length < 2) {
+			System.err.println("Usage: demo NullWritableDemo <in> <out>");
+			System.exit(2);
+		}
+		
+		Job job = Job.getInstance(conf, NullWritableDemo.class.getSimpleName());
+		//指定jar包位置
+		job.setJarByClass(NullWritableDemo.class);
+		//指定mapper
+		job.setMapperClass(NullValueMapper.class);
+		//指定reducer
+		job.setReducerClass(NullValueReducer.class);
+		
+		//reducer输出：
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(NullWritable.class);
+		
+		//首先判断input目录是否已经存在，如果存在，先删除
+		boolean exit = FileSystem.get(conf).exists(new Path(otherArgs[1]));
+		if(exit){
+			FileSystem.get(conf).delete(new Path(otherArgs[1]), true);
+			System.out.println("delete:"+otherArgs[1]);
+		}
+		
+		/**
+		 * 指定input位置：
+		 * 如果有多个input，可以for (int i = 0; i < otherArgs.length - 1; ++i) 
+		 */
+		FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
+		//指定output位置
+		FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
+		//提交到yarn，一直等程序结束运行
+        return job.waitForCompletion(true) ? 0 : 1;
+	}
+	
+	public static void main(String[] args) {
+		try {
+			int res = ToolRunner.run(new NullWritableDemo(), args);
+			System.exit(res);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}  
+	}
+}
+class NullValueMapper extends Mapper<LongWritable, Text, Text, NullWritable>{
+	@Override
+	protected void map(LongWritable key, Text value, Context context)
+			throws IOException, InterruptedException {
+		context.write(value, NullWritable.get());
+	}
+}
+class NullValueReducer extends Reducer<Text, NullWritable, Text, NullWritable>{
+	@Override
+	protected void reduce(Text key, Iterable<NullWritable> values,Context context) 
+			throws IOException, InterruptedException {
+		context.write(key, NullWritable.get());
+	}
+}
---
+++ celloud.hadoop/src/main/java/dashuju/celloud/hadoop/demo3/NullWritableTest.java
@@ -0,0 +1,109 @@
+package dashuju.celloud.hadoop.demo3;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mrunit.mapreduce.MapDriver;
+import org.apache.hadoop.mrunit.mapreduce.MapReduceDriver;
+import org.apache.hadoop.mrunit.mapreduce.ReduceDriver;
+import org.apache.hadoop.mrunit.types.Pair;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.google.common.collect.Lists;
+
+/**
+ * MRUnit测试
+ * 参考：http://blog.csdn.net/sunnyyoona/article/details/53523652
+ * @author Administrator
+ *
+ */
+public class NullWritableTest {
+    private MapDriver mapDriver;
+    private ReduceDriver reduceDriver;
+    private MapReduceDriver mapReduceDriver;
+    
+    @Before
+    public void setUp(){
+    	NullValueMapper mapper = new NullValueMapper();
+        mapDriver = MapDriver.newMapDriver(mapper);
+        
+        NullValueReducer reducer = new NullValueReducer();
+        reduceDriver = ReduceDriver.newReduceDriver(reducer);
+        mapReduceDriver = MapReduceDriver.newMapReduceDriver(mapper, reducer);
+    }
+    
+    @Test
+    public void testMapper() throws IOException {
+        Text text1 = new Text("2012-3-7 c");
+        Text text2 = new Text("2012-3-1 a");
+        Text text3 = new Text("2012-3-2 c");
+        Text text4 = new Text("2012-1-7 d");
+        Text text5 = new Text("2012-3-1 c");
+        mapDriver
+        .withInput(new LongWritable(), text1)
+        .withInput(new LongWritable(), text2)
+        .withInput(new LongWritable(), text3)
+        .withInput(new LongWritable(), text4)
+        .withInput(new LongWritable(), text5)
+        ;
+        mapDriver
+        .withOutput(new Text("2012-3-7 c"), NullWritable.get())
+        .withOutput(new Text("2012-3-1 a"), NullWritable.get())
+        .withOutput(new Text("2012-3-2 c"), NullWritable.get())
+        .withOutput(new Text("2012-1-7 d"), NullWritable.get())
+        .withOutput(new Text("2012-3-1 c"), NullWritable.get())
+        ;
+        mapDriver.runTest();
+        // 输出
+        List<Pair> expectedOutputList = mapDriver.getExpectedOutputs();
+        for(Pair pair : expectedOutputList){
+            System.out.println(pair.getFirst() + " --- " + pair.getSecond()); 
+        }
+    }
+    
+    @Test
+    public void testReducer() throws IOException {
+        List<NullWritable> values = Lists.newArrayList();
+        values.add(NullWritable.get());
+        values.add(NullWritable.get());
+        values.add(NullWritable.get());
+        
+        
+        reduceDriver.withInput(new Text("2012-3-1 c"), values).
+        withOutput(new Text("2012-3-1 c"), NullWritable.get()).
+        runTest();
+        // 输出
+        List<Pair> expectedOutputList = reduceDriver.getExpectedOutputs();
+        for(Pair pair : expectedOutputList){
+            System.out.println(pair.getFirst() + " --- " + pair.getSecond());
+        }
+    }
+    
+    /**
+     * 按照字典排序
+     * @throws IOException
+     */
+    @Test
+    public void testMR() throws IOException{
+        Text text1 = new Text("hello you hello me");
+        Text text2 = new Text("how old are you");
+        mapReduceDriver.withInput(new LongWritable(), text1).
+        withInput(new LongWritable(), text2).
+        withOutput(new Text("are"), new LongWritable(1)).
+        withOutput(new Text("hello"), new LongWritable(2)).
+        withOutput(new Text("how"), new LongWritable(1)).
+        withOutput(new Text("me"), new LongWritable(1)).
+        withOutput(new Text("old"), new LongWritable(1)).
+        withOutput(new Text("you"), new LongWritable(2)).
+        runTest();
+        // 输出
+        List<Pair> expectedOutputList = mapReduceDriver.getExpectedOutputs();
+        for(Pair pair : expectedOutputList){
+            System.out.println(pair.getFirst() + " --- " + pair.getSecond()); 
+        }
+    }
+}
